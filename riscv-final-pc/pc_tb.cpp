13d4c1fe2298c866351c0a3baf61b60fbce2f1bf
Arnav Kohli <arnavkohli@dyn3182-118.wlan.ic.ac.uk>
2022-12-02 17:13:19 +0000

    finished pc block testbench

---
+++ riscv-final-pc/pc_tb.cpp
@@ -0,0 +1,40 @@
+#include "VPC_top.h"
+#include "verilated.h"
+#include "verilated_vcd_c.h" 
+
+int main(int argc,char **argv, char **env){
+    int i;
+    int clk;
+
+    Verilated::commandArgs(argc, argv);
+    //init top verilog instance 
+    VPC_top* top = new VPC_top;
+    // init trace dump
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* tfp= new VerilatedVcdC;
+    top->trace (tfp,99);
+    tfp->open ("PC_top.vcd");
+
+    // initialize simulation inputs
+    top->clk = 1;
+    top->rst = 1;
+    top->PCsrc = 0; // when in mode 0, the rom increments by 4. To test the ImmOp branch operation, set this to 1 and change the variable below to the increment/decrement you want to test.
+    top->ImmOp = 0xFFF; // this should increment by -1 when in branch mode. It is greater than 8 bits and proves that the PC block works.
+    
+    //run simulation for many clock cycles
+    for (i=0; i<300; i++){ // clock cycles - used only 300 to limit size of VCD file
+            //dump variables into VCD file and toggle clock
+            for (clk=0; clk<2; clk++) {
+                tfp->dump (2*i+clk);
+                top->clk = !top->clk;
+                top->eval();
+            }
+
+            // change input stimuli
+            top->rst = ((i-2) % 10 == 0) | (i<2);   // resets after every 12th clock cycle
+            if (Verilated::gotFinish())  exit(0);
+    }
+    tfp->close();
+    exit(0);
+
+}
