0c83854bf9efab0c22a2bc9353fc83b151a71597
Arnav Kohli <arnavkohli@dyn3182-118.wlan.ic.ac.uk>
2022-12-02 17:11:53 +0000

    finished top module for pc.

---
+++ riscv-final-pc/top_pc.sv
@@ -0,0 +1,20 @@
+module top_pc #(
+        parameter ADDRESS_WIDTH = 12,
+    DATA_WIDTH = 8
+)(
+  input logic [31:0] ImmOp,
+  input logic                     PCsrc,
+  input logic                     clk,
+  input logic                     rst,
+  output logic [DATA_WIDTH-1:0]   instr
+);
+
+  logic [ADDRESS_WIDTH-1:0] internalImmOp = ImmOp[11:0];
+
+  logic [ADDRESS_WIDTH-1:0] PC;
+
+pcreg pcReg(internalImmOp, PCsrc, clk, rst, PC);
+
+rom pcRom(PC, instr);
+
+endmodule
