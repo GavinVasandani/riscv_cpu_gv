a4a6c665e366ba6f51c9668c9e918b1c9f0f762a
Arnav Kohli <arnavkohli@dyn3182-118.wlan.ic.ac.uk>
2022-12-02 16:59:19 +0000

    completed rom file.

---
+++ riscv-final-pc/rom.sv
@@ -0,0 +1,22 @@
+module rom #(
+    parameter ADDRESS_WIDTH = 12, // we use only 12 bits (from 000 to FFF) to accommodate the entire program. See readme for more details.
+    DATA_WIDTH = 8 // data width of each instruction has to be 8. This allows for 4 bytes to be concatenated to form one word.
+)(
+    input logic [ADDRESS_WIDTH-1:0] PC,
+    output logic [31:0]   instr
+);
+
+logic [DATA_WIDTH-1:0] rom_array [2**ADDRESS_WIDTH-1:0];
+
+initial begin
+    $display("Loading rom.");
+    $readmemh("instructions.mem", rom_array);
+    $display("Rom successfully loaded.");
+end;
+
+// asynchronous assignment, should be blocking
+assign instr = {rom_array[PC], rom_array[PC+1], rom_array[PC+2], rom_array[PC+3]};
+// byte addressing causes increments of 4.
+
+
+endmodule
