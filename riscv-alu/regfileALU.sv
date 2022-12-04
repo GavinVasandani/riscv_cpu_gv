1e820ab2d01fae6794119af49042aac7a73e30a3
GavinVasandani <89224315+GavinVasandani@users.noreply.github.com>
2022-12-04 14:49:06 -0900

    added changes to ALU code

--- riscv-alu/regfileALU.sv
+++ riscv-alu/regfileALU.sv
@@ -1,7 +1,7 @@
 //Creating ALU module
 //ALU implements all ALU_controls
 module regFileALU # (
-    parameter Address_Width = 5,
+    parameter //Address_Width = 5,
               //ALU_Instruction_Width = 1;
               Data_Width = 32 //Data_Width aka number of bits for instruction word doesn't need to correlate to size of main mem
 ) (
@@ -9,7 +9,7 @@ module regFileALU # (
     input logic [Data_Width-1:0] op1, //input op1 into ALU which is output content from a register
     input logic [Data_Width-1:0] op2, //input op2 into ALU which is either 32-bit Imm or 32-bit content from register
     //input logic [ALU_Instruction_Width-1:0] ALU_ctrl,
-    input logic ALU_ctrl,
+    input logic [3:0] ALU_ctrl, //4 bit ALU_ctrl
     output logic [Data_Width-1:0] ALUout, //output of ALU is same size as ALU inputs
     output logic eq //Eq is probably carry out flag
 );
@@ -18,28 +18,28 @@ module regFileALU # (
 
 always_comb begin //ALU implements different arithmetic/logic based on opcode input
     case (ALU_ctrl) //ALU is combinational, async so normal = is fine for variable assignments
-        1'b0000: begin//Add: //ALU implements add for lw and sw instructions also, the output operand is just used for something else
+        4'b0000: begin//Add: //ALU implements add for lw and sw instructions also, the output operand is just used for something else
         //but that's irrelevant to ALU:
             ALUout = op1 + op2;
             eq = 0;
         end
-        1'b0001: begin//Sub: //Used for beq, bne and sub instruction, eq output only used for 
+        4'b0001: begin//Sub: //Used for beq, bne and sub instruction, eq output only used for 
             ALUout = op1 - op2; //order was determined from given instruction set/manual
             //determining eq:
-            if((op1 - op2)==0) eq = 1;
+            if((op1 - op2)==0) eq = 1; //can't do if(ALUout==0) as all logic is done simultaneously so ALUout isn't assigned op1-op2 when calling ALUout for this line.
             else eq = 0;
         end
-        1'b0010: begin//AND instruction where we do bitwise AND, so compare (AND) bit at each position of op1 and op2 and output to that bit in rd
+        4'b0010: begin//AND instruction where we do bitwise AND, so compare (AND) bit at each position of op1 and op2 and output to that bit in rd
             ALUout = op1 & op2;
             //eq is don't care:
             eq = 0;
         end
-        1'b0011: begin //OR instruction
+        4'b0011: begin //OR instruction
             ALUout = op1 | op2;
             //eq is don't care:
             eq = 0;
         end
-        1'b0101: begin //SLT and BLT instruction so compare operands and output 1 based on which is greater:
+        4'b0101: begin //SLT and BLT instruction so compare operands and output 1 based on which is greater:
             //Setting rd = 1 (if rs1<rs2), rd = 0 (if rs1>rs2) so ALUout must be modified and ALUout written to rd: 
             if(op1 < op2) begin 
                 ALUout = 1;
@@ -52,48 +52,34 @@ always_comb begin //ALU implements different arithmetic/logic based on opcode in
                 eq = 0;
             end
         end
-        1'b1000: begin //BGE instruction:
+        4'b1000: begin //BGE instruction:
             if (op1 >= op2) eq = 1;
             else eq = 0;
             //ALUout is don't care
             ALUout = 0;
         end
         //ALU_ctrl: 1'b100 shift left logical (SLL) operation also for SLLI where op2 is an immediate but that's assigned before entering ALU
-        1'b0100: begin
+        4'b0100: begin
             ALUout = op1 << op2;
             //eq is don't care:
             eq = 0;
         end
         //ALU_ctrl: 1'110 shift right logical (SRL) operation also for SRLI where op2 is an immediate but that's assigned before entering ALU
-        1'b0110: begin
+        4'b0110: begin
             ALUout = op1 >> op2;
             //eq is don't care:
             eq = 0;
         end
-        1'b0111: begin //SRA instruction: so shift right and maintaining signed behaviour
+        4'b0111: begin //SRA instruction: so shift right and maintaining signed behaviour
             ALUout = op1 >>> op2;
             //eq is don't care:
             eq = 0;
         end
-        1'b1001: begin //XOR instruction:
+        4'b1001: begin //XOR instruction:
             ALUout = op1 ^ op2; 
             //eq is don't care:
             eq = 0;
         end
-        /*
-        1'b0: begin//Add Immediate:
-            ALUout = op1 + op2; // eq is irrelevant here as instruction for adding the two inputs would not need to consider/evaluate eq,
-            so eq can be any value here
-            //Similarly for bne, ALUout is irrelevant as only eq will be evaluated to check whether to jump to label or not
-            eq = 0; //technically eq is don't care, and as branch = 0 for non-branching instructions then regardless of eq value we don't branch
-        end
-        1'b1: begin//bne instruction:
-            ALUout = 0; //ALUout is don't care, but if we assign it some value as instruction is bne/beq then regFile write = 0 and memWrite = 0 so no effect of ALUout
-            if((op1-op2)==0) eq = 1; //so if op1-op2 is 0 then means the operands are equal so eq = 1, else operands are not equal so eq=0
-            else eq = 0;
-            //ALUOut is don't care.
-        end
-        */
         default: $display("Instruction not detected.");
     endcase
 end
