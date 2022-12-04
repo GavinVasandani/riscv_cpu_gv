f092c01aba1d5d262cd0d1b4e6b80302845f9ece
Arnav Kohli <arnavkohli@dyn3182-118.wlan.ic.ac.uk>
2022-12-04 22:35:02 +0000

    minor semantic changes

--- riscv-final-pc/top_pc.sv
+++ riscv-final-pc/top_pc.sv
@@ -1,13 +1,12 @@
 module top_pc #(
         parameter ADDRESS_WIDTH = 12,
-    DATA_WIDTH = 8,
-    OUTPUT_WIDTH = 32
+    DATA_WIDTH = 32,
 )(
-  input logic [OUTPUT_WIDTH-1:0] ImmOp,
+  input logic [DATA_WIDTH-1:0] ImmOp,
   input logic                     PCsrc,
   input logic                     clk,
   input logic                     rst,
-  output logic [OUTPUT_WIDTH-1:0]   instr
+  output logic [DATA_WIDTH-1:0]   instr
 );
 
   logic [ADDRESS_WIDTH-1:0] internalImmOp = ImmOp[11:0];
