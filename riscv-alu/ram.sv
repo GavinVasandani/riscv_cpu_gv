44c33de572e04ac59596fd5e8b2f758cc49e0132
GavinVasandani <89224315+GavinVasandani@users.noreply.github.com>
2022-12-04 16:54:44 -0900

    Added comments to ram file

--- riscv-alu/ram.sv
+++ riscv-alu/ram.sv
@@ -1,5 +1,6 @@
 module ram #(
     parameter ADDRESS_WIDTH = 12, // needs to be 12 bit wide, look at memory map of final project for reasoning
+    //memory addresses reserved from data memory is 0x00001000 to 0x00001FFF, so 12 bit address width, so 4096 referencable mem locations.
     DATA_WIDTH = 32 // 32 bit value stored in mem location of RAM - since it's byte addressed we store data in 4 addresses - confirm with GTA
 )(
     input logic clk,
@@ -12,11 +13,14 @@ module ram #(
 logic [DATA_WIDTH-1:0] ram_array [2**ADDRESS_WIDTH-1:0];
 
 // you don't need to initialize a ram array with 0s, there is no need for loadmemh operation
+//yeah but need to load in some initial values to the ram so probably do need a loadmemh operation
 
 assign RD = {ram_array[A], ram_array[A+1], ram_array[A+2], ram_array[A+3]}; // asynchronous read - have an iisue here, want to confirm with the GTA
+//if each address stores a 32-bit word then RD would be 128 bits, so each address needs to store 1 byte, so 8 bit and then combined gives 32-bit word
 
 always_ff @(posedge clk) begin
     if (WE) // synchronous write
+    //last 2 bytes (16 bits) of word 
         ram_array[A] <= WD[];
         ram_array[A+1] <= WD[];
         ram_array[A+2] <= WD[15:8];
--- riscv-alu/topLevelALU.sv
+++ riscv-alu/topLevelALU.sv
@@ -1,21 +1,24 @@
 module topLevelALU# (
     parameter 
               // NumberOfReg = 32,
-              Address_Width = 5, //32 registers so address size is 5 bits
+              Address_Width_RegFile = 5, //32 registers so address size is 5 bits
+              Address_Width_RAM = 12,
               Data_Width = 32
               //ALU_Instruction_Width = 10;
 ) (
 //Interface Signals
     input logic clk,
-    input logic [Address_Width-1:0] rs1, //32 registers so 5 bit address, rs1 is 1st source reg, arithmetic input
-    input logic [Address_Width-1:0] rs2, //rs2 is 2nd source reg, arithmetic input
-    input logic [Address_Width-1:0] rd, //rd is destination reg to store arithmetic output
-    input logic en, //Enable to determine whether we can write to register file or not
+    input logic [Address_Width_RegFile-1:0] rs1, //32 registers so 5 bit address, rs1 is 1st source reg, arithmetic input
+    input logic [Address_Width_RegFile-1:0] rs2, //rs2 is 2nd source reg, arithmetic input
+    input logic [Address_Width_RegFile-1:0] rd, //rd is destination reg to store arithmetic output
+    input logic regFileWen, //Enable to determine whether we can write to register file or not
     //change: input logic [Data_Width-1:0] din, //Data in which takes in ALU output to write to register rd. Registers hold 32 bit word so din is 32 bits
     input logic ALUSrc,
     input logic [Data_Width-1:0] ImmOp,
     //input logic [ALU_Instruction_Width-1:0] ALU_ctrl;
     input logic [3:0] ALU_ctrl,
+    input logic MemWrite, //WE
+    input logic ResultSrc,
 //Outputs
     //change: output logic [Data_Width-1:0] ALUout, //output of ALU is same size as ALU inputs
     output logic eq,
@@ -26,25 +29,28 @@ module topLevelALU# (
 logic [Data_Width-1:0] rd1;
 logic [Data_Width-1:0] rd2;
 logic [Data_Width-1:0] ALUOp2;
+logic [Data_Width-1:0] ALUout;
 
 //Din is output of ALU and input of regfile:
 logic [Data_Width-1:0] dinTest;
+logic [Data_Width-1:0] ReadData;
 
 //Initializing objects of the different modules and linking them
+//.variablefromClass(variablefromTop)
 //regFile regFile1 (clk, rs1, rs2, rd, en, din, rd1, rd2, a0);
 regFile regFile1 (
     .clk(clk),
     .rs1(rs1),
     .rs2(rs2), 
     .rd(rd), 
-    .en(en), 
-    .din(dinTest), 
+    .en(regFileWen), 
+    .din(dinTest), //register file input is dinTest which was resultSrc mux output
     .rd1(rd1), 
     .rd2(rd2),
-    .a0(a0)
+    .a0(a0) //check if needed
 );
 //regFileMux mux1 (rd2, ImmOp, ALUSrc, ALUOp2);
-regFileMux mux1 (
+regFileMux mux1 ( //Mux to determine ALU Op2
     .regOp2(rd2), 
     .ImmOp(ImmOp), 
     .ALUSrc(ALUSrc), 
@@ -57,6 +63,21 @@ regFileALU alu1 (
     .ALU_ctrl(ALU_ctrl), 
     .ALUout(dinTest), 
     .eq(eq)
+);
+
+ram ram1 (
+    .clk(clk),
+    .WE(MemWrite),
+    .A(ALUout[Address_Width_RAM-1:0]),
+    .WD(rd2),
+    .RD(ReadData)
+);
+
+resultSrcMux resultSrcMux1 (
+    .ALUResult(ALUout),
+    .ReadData(ReadData),
+    .ResultSrc(ResultSrc),
+    .Result(dinTest)
 );
 
 endmodule
