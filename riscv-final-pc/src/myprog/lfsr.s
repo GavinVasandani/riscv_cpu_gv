df0f58c6cef55c26eb2eb63f00bdb04b7466e1a2
Arnav Kohli <arnavkohli@dyn3177-193.wlan.ic.ac.uk>
2022-12-05 18:28:30 +0000

    initiated assembly program for lfsr

---
+++ riscv-final-pc/src/myprog/lfsr.s
@@ -0,0 +1,16 @@
+.text
+.globl main
+main:
+    addi a0, zero, 0x0 # a0 is used for output
+    addi a1, zero, 0x1 # a1 is the lfsr, we start with 1
+mloop:
+    addi a0, a1, 0x0 # load output with answer
+    slli a1, t1, 0x1d # shift left 29 times to get rid of the 4th bit
+    srli a1, a1, 0x1c # a1 contains the first three bits of the number (shift right 28 times) as bits 1, 2 and 3 instead of 0, 1, 2 (x3x2x10)
+    slli a2, t1, 0x1e # shift left 30 times to get rid of the 3rd and 4th bits
+    srli a2, a2, 0x1f # a2 contains the 3rd bit of the number (shift right 31 times)
+    srli a3, t1, 0x3 # a3 contains the 4th bit of the number
+    xor t2, a3, a2 # randomization step (n)
+    add a1, a1, t2 # reconstruct the number : x3x2x1n where n is the new bit from the randomization
+    jal zero, mloop # continue forever - this is temporary
+
