6ceb2e28ce8d17438b509645c26b674bdf84b620
Arnav Kohli <arnavkohli@dyn3187-61.wlan.ic.ac.uk>
2022-12-06 18:20:35 +0000

    changed lfsr to 7 bit wide

--- riscv-final-pc/src/myprog/lfsr.s
+++ riscv-final-pc/src/myprog/lfsr.s
@@ -1,17 +1,15 @@
-.text
-.globl main
 main:
     addi s2, zero, 0x0 # s2 is used by delay in subsequent modules
-    addi a1, zero, 0x1 # a1 is the lfsr, we start with 1
+    addi a1, zero, 0x55 # a1 is the lfsr, we start with 1
 mloop:
     addi s2, a1, 0x0 # load output with answer
     addi t1, s2, 0x0 # initialize dummy register for operation
-    slli a1, t1, 0x1d # shift left 29 times to get rid of the 4th bit
-    srli a1, a1, 0x1c # a1 contains the first three bits of the number (shift right 28 times) as bits 1, 2 and 3 instead of 0, 1, 2 (x3x2x10)
-    slli a2, t1, 0x1d # shift left 29 times to get rid of the 4th bit
-    srli a2, a2, 0x1f # a2 contains the 3rd bit of the number (shift right 32 times)
-    srli a3, t1, 0x3 # a3 contains the 4th bit of the number
+    slli a1, t1, 0x1A # shift left 26 times to get rid of the 7th bit
+    srli a1, a1, 0x19 # a1 contains the first six bits of the number (shift right 25 times) as bits 1, 2 and 3 instead of 0, 1, 2 (x3x2x10)
+    slli a2, t1, 0x1c # shift left 28 times to get rid of the 4th bit
+    srli a2, a2, 0x1f # a2 contains the 4th bit of the number (shift right 31 times)
+    srli a3, t1, 0x6 # a3 contains the 7th bit of the number
     xor t2, a3, a2 # randomization step (n)
     add a1, a1, t2 # reconstruct the number : x3x2x1n where n is the new bit from the randomization
     jal zero, mloop # continue forever - this is temporary
-
+    
