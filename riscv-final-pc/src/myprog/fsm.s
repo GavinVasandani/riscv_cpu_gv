4aec4ee51122a7499a9d00dd1a870a162c9c8918
Arnav Kohli <arnavkohli@dyn3187-61.wlan.ic.ac.uk>
2022-12-06 21:10:43 +0000

    ensured jumps are present, requires jal and jalr operations

--- riscv-final-pc/src/myprog/fsm.s
+++ riscv-final-pc/src/myprog/fsm.s
@@ -3,13 +3,13 @@
 # registers in use: s2, a1, a2, a3
 # a5 will be used to store the delay value
 # a6 will be used to check if the program has already run once. If so, it will stop running
-# s4 is used to store the value of PC that initiates the checkdelay loop
+# ra is used to store the value of PC that holds the return from subroutine value
 main:
     addi a0, zero, 0x0 # initialization step
-    addi s4, zero, 0x0 
+    addi ra, zero, 0x0 # reset subroutine register
     bne a6, zero, main
-; setdelay:
-;     addi a5, zero, 0x5 # set delay to 5 for testing purposes. REMOVE THIS FROM FINAL CODE. DELAY COMES FROM LFSR
+# setdelay:
+#    addi a5, zero, 0x5 # set delay to 5 for testing purposes. REMOVE THIS FROM FINAL CODE. DELAY COMES FROM LFSR
 lightloop:
     addi a0, zero, 0x1 # first light
     addi a0, zero, 0x3 # second light
@@ -20,12 +20,13 @@ lightloop:
     addi a0, zero, 0x7F # seventh light
     addi a0, zero, 0xFF # eighth light
 checkdelay:
-    bne a5, zero, delay # branch to delay if the delay register hasn't hit 0 yet.
+    jal ra, delay # jump to the delay subroutine and store return value in ra
     addi a6, zero, 0x1 # terminate program - since you only reach here if the delay has reached 0
     beq a5, zero, main # this jumps to the beginning and ensures the program terminates after resetting the lights to 0.
 delay:
     addi a5, a5, -1 # subtract 1 from delay
-    jal s4, checkdelay # call the subroutine that checks the delay register
+    bne a5, zero, delay # loop till delay hits 0
+    jalr x0, x1, 0 # return from subroutine - equivalent to the ret instruction - initiates program termination
 
 # look at f1 starting light code for reasoning behind design decisions
 # suggested modifications/ questions to ask to the GTA:
