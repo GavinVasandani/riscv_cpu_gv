0bee898700bbb3732b53cb356986717c4fbc36d6
Arnav Kohli <arnavkohli@dyn3179-56.wlan.ic.ac.uk>
2022-12-07 18:02:37 +0000

    added code for delay

--- riscv-final-pc/src/myprog/lfsr.s
+++ riscv-final-pc/src/myprog/lfsr.s
@@ -1,9 +1,9 @@
 main:
-    addi s2, zero, 0x0 # s2 is used by delay in subsequent modules
-    addi a1, zero, 0x55 # a1 is the lfsr, we start with 1
+    addi a5, zero, 0x0 # s2 is used by delay in subsequent modules
+    addi a1, zero, 0x1 # a1 is the lfsr, we start with 1
 mloop:
-    addi s2, a1, 0x0 # load output with answer
-    addi t1, s2, 0x0 # initialize dummy register for operation
+    addi a5, a1, 0x0 # load output with answer
+    addi t1, a5, 0x0 # initialize dummy register for operation
     slli a1, t1, 0x1A # shift left 26 times to get rid of the 7th bit
     srli a1, a1, 0x19 # a1 contains the first six bits of the number (shift right 25 times) as bits 1, 2 and 3 instead of 0, 1, 2 (x3x2x10)
     slli a2, t1, 0x1c # shift left 28 times to get rid of the 4th bit
@@ -11,5 +11,5 @@ mloop:
     srli a3, t1, 0x6 # a3 contains the 7th bit of the number
     xor t2, a3, a2 # randomization step (n)
     add a1, a1, t2 # reconstruct the number : x3x2x1n where n is the new bit from the randomization
-    jal zero, mloop # continue forever - this is temporary
+    beq a7, zero, mloop # loop till trigger is asserted - trigger is stored in a7 or x17
     
