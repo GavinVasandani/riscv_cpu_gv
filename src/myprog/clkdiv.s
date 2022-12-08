c86684779a71ac9ca18843a9b1333c10f2f97cbf
Arnav Kohli <arnavkohli@dyn3177-153.wlan.ic.ac.uk>
2022-12-08 21:55:39 +0000

    testing of jalr complete - fix sub and x0 bugs.

--- src/myprog/clkdiv.s
+++ src/myprog/clkdiv.s
@@ -1,7 +1,8 @@
 .text
 .globl main
 main:
-    addi t5, t5, 0x1 # for subtraction
+    addi a0, zero, 0x0
+    addi t5, zero, 0x1 # for subtraction
     addi a6, zero, 0x0E # set clkdiv to roughly 14 cycles - should be initialized at the start of main program
     jal ra, mloop 
     addi a0, a0, 0x1 # if a0 contains 1 the tests have passed
@@ -11,6 +12,7 @@ mloop:
     bne a6, zero, mloop # loop till you hit zero
     jalr x0, ra, 0 # ret instruction
 end:
+    jal ra, main
 
 
     
