9ba29b02f1df5b4c11d5584657f0eb22c538f94a
Arnav Kohli <arnavkohli@dyn3177-153.wlan.ic.ac.uk>
2022-12-08 20:32:19 +0000

    finished changes in pc for jalr (ret) operation.

--- src/myprog/clkdiv.s
+++ src/myprog/clkdiv.s
@@ -2,7 +2,14 @@
 .globl main
 main:
     addi a6, zero, 0x0E # set clkdiv to roughly 14 cycles - should be initialized at the start of main program
+    jal ra, mloop 
+    addi a0, a0, 0x1 # if a0 contains 1 the tests have passed
+    jal ra, end
 mloop:
     addi a6, a6, -1 # decrement by one
     bne a6, zero, mloop # loop till you hit zero
+    jalr x0, ra, 0 # ret instruction
+end:
+
+
     
