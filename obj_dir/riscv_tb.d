7e7db49f5cdb42088d79332545d6e9bc0f47e833
Johnny18823 <jw3621@ic.ac.uk>
2022-12-08 10:28:54 +0000

    pipeline_regiser

--- obj_dir/riscv_tb.d
+++
@@ -1,9 +0,0 @@
-riscv_tb.o: ../riscv_tb.cpp Vtop.h \
-  /opt/homebrew/Cellar/verilator/5.002/share/verilator/include/verilated.h \
-  /opt/homebrew/Cellar/verilator/5.002/share/verilator/include/verilatedos.h \
-  /opt/homebrew/Cellar/verilator/5.002/share/verilator/include/verilated_config.h \
-  /opt/homebrew/Cellar/verilator/5.002/share/verilator/include/verilated_types.h \
-  /opt/homebrew/Cellar/verilator/5.002/share/verilator/include/verilated_funcs.h \
-  /opt/homebrew/Cellar/verilator/5.002/share/verilator/include/verilated_vcd_c.h \
-  /opt/homebrew/Cellar/verilator/5.002/share/verilator/include/verilated_trace.h \
-  /opt/homebrew/Cellar/verilator/5.002/share/verilator/include/verilated_trace_defs.h
