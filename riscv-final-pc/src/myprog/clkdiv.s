73ea589fcb001cff3c50d18eb41daecfe1b8c5ae
Arnav Kohli <arnavkohli@dyn3177-153.wlan.ic.ac.uk>
2022-12-08 21:42:57 +0000

    tested jalr instruction using clkdiv

--- riscv-final-pc/src/myprog/clkdiv.s
+++ riscv-final-pc/src/myprog/clkdiv.s
@@ -1,12 +1,13 @@
 .text
 .globl main
 main:
+    addi t5, t5, 0x1 # for subtraction
     addi a6, zero, 0x0E # set clkdiv to roughly 14 cycles - should be initialized at the start of main program
     jal ra, mloop 
     addi a0, a0, 0x1 # if a0 contains 1 the tests have passed
     jal ra, end
 mloop:
-    addi a6, a6, -1 # decrement by one
+    sub a6, a6, t5 # decrement by one
     bne a6, zero, mloop # loop till you hit zero
     jalr x0, ra, 0 # ret instruction
 end:
