7175dfeb6af5e79ac42a6e877148d05b21c3b28b
GavinVasandani <89224315+GavinVasandani@users.noreply.github.com>
2022-12-13 11:11:35 +0000

    Added comments and made code more simpler

--- rtl/riscv-alu/ram_cache.sv
+++ rtl/riscv-alu/ram_cache.sv
@@ -27,16 +27,7 @@ module ram_cache # (
 //Initializing Cache and cache variables:
 logic [CACHE_DATA_WIDTH-1:0] cache_array [2**CACHE_ADDRESS_WIDTH-1:0];
 
-/* Cache is assumed to be filled with 0s
-initial begin
-    $display("Loading cache.");
-    $readmemh("cachedata.mem", cache_array); //64 mem locations of 41 bits each value
-    $display("Cache successfully loaded.");
-end;
-*/
-
 //Initializing RAM and RAM variables:
-//logic [BYTE_WIDTH-1:0] ram_array [2**RAM_ADDRESS_WIDTH-1:0]; //each mem location of array stores a byte-width so 8 bits
 logic [BYTE_WIDTH-1:0] ram_array [2**RAM_ADDRESS_WIDTH-1:0];
 
 initial begin
@@ -45,19 +36,12 @@ initial begin
     $display("Ram successfully loaded.");
 end;
 
-//Cache to refer to:
-//logic [CACHE_ADDRESS_WIDTH-1:0] cache_address;
-//assign cache_address = A[7:2];
-//assign cache_address = A[(1+CACHE_ADDRESS_WIDTH):2]; //so 6 bit for cach e address in this case
-
-//Word at cache: //Check if allowed to do values chronologically
+//Word at cache:
 logic [CACHE_DATA_WIDTH-1:0] cache_data;
 assign cache_data = cache_array[A[7:2]];
 
 logic flagMiss; //1-Miss, 0-Hit
-//assign cache_data = cache_array[cache_address]; //this is entire 57 bit data stored in cache
 
-//Checking if writing to RAM instruction: No need to write to Cache if writing to RAM
 always_ff @(posedge clk) begin 
     //synchronous write
     if (WE) begin
@@ -71,12 +55,12 @@ always_ff @(posedge clk) begin
                 end
                 2'b01: begin //write byte to mem location given by A
                     ram_array[A] <= WD[7:0];
-                    cache_array[A[7:2]] <= {1'b1, A[15:8], {24{1'b0}}, WD[7:0]};
+                    cache_array[A[7:2]] <= {1'b1, A[15:8], {24{1'b0}}, WD[7:0]}; //writing byte to cache
                 end
                 2'b10: begin
                     ram_array[A] <= WD[7:0]; //LS Byte
                     ram_array[A+1] <= WD[15:8];
-                    cache_array[A[7:2]] <= {1'b1, A[15:8], {16{1'b0}}, WD[15:8], WD[7:0]};
+                    cache_array[A[7:2]] <= {1'b1, A[15:8], {16{1'b0}}, WD[15:8], WD[7:0]}; //writing halfword to cache
                 end
                 default: $display("No dataType selected. Please choose word, byte or halfword.");
             endcase
@@ -84,38 +68,37 @@ always_ff @(posedge clk) begin
 end
 
 always_comb begin //new instruction comes with new clk cycle, so flagMiss can still be used at clk posedge for current instruction
-    if (!WE) begin //So read instruction so cache is used
-        //1. Refer to cache to see if data at A is in cache:
-        //Combinational/immediate read from cache:
+    if (!WE) begin //Read instruction so cache is checked first
         if(cache_data[40]) begin //so if V is 1
-            if(cache_data[39:32]==A[15:8]) begin //compare 24 bit tag in cache to A[31:5] to see if cache contains correct mem location value
-                //if true then output data in cache_data
+            if(cache_data[39:32]==A[15:8]) begin //compare tag
                 assign RD = cache_data[31:0]; //output immediately that DataOut is 31 bit data stored in cache, as its read its async
-                assign flagMiss = 1'b0;
+                assign flagMiss = 1'b0; //hit
             end
-            else begin
+            else begin //tag doesn't match so miss, so read from RAM
                 assign RD = {ram_array[A+3], ram_array[A+2], ram_array[A+1], ram_array[A]};
-                assign flagMiss = 1'b1;
+                assign flagMiss = 1'b1; //miss
             end
         end
-        else begin //in any other case: wrong tag or V is 0, RD is assigned to RAM value
+        else begin //0 V-bit so miss, so read fom RAM
             assign RD = {ram_array[A+3], ram_array[A+2], ram_array[A+1], ram_array[A]};
-            assign flagMiss = 1'b1;
+            assign flagMiss = 1'b1; //miss
         end
     end
-end //these flagMiss values are used immediately when clock is posedge 
+end
 
 always_ff @(posedge clk) begin
-    if (flagMiss) begin //combinational read from ram_array for ram_array[A+3],...,ram_array[A], so done immediately when clk posedge
+    if (flagMiss) begin //Write to cache if miss
       cache_array[A[7:2]] <= {1'b1, A[15:8], ram_array[A+3], ram_array[A+2], ram_array[A+1], ram_array[A]};  
     end
 end
 
+//For debugging
 assign RAM_array_value = ram_array[3];
 assign cache_array_value = cache_array[A[7:2]];
 
 endmodule
 
 
-//Currently RAM only outputs word and cache only stores word, no byte or halfword.
-//RAM can store word, half word or byte.
+//Currently RAM-cache only outputs word and cache stores word, byte or halfword.
+//RAM-cache can store word, half word or byte.
+//Add functionality for cache outputting halfword or byte.
